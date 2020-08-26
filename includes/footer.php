979a7ad81c58e4b064f16eb7b400da26c863492d
Avni Mishra <avnimishra679@gmail.com>
2020-08-26 21:40:16 +0530

    created

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,8 @@
 <footer>
     <div class="container">
         <center>
+
+            <p> WELCOME</p>
             <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</p>    
     </center>   
     </div>
