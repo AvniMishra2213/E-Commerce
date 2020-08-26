1a5ef804edc0017cd1440760745b180aec2bf52d
Saumya Singh <43070615+SAUMYA1519@users.noreply.github.com>
2020-08-26 11:59:29 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -10,21 +10,6 @@
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav navbar-right">
-                <?php
-                if (isset($_SESSION['email'])) {
-                    ?>
-                    <li><a href = "cart.php"><span class = "glyphicon glyphicon-shopping-cart"></span> Cart </a></li>
-                    <li><a href = "settings.php"><span class = "glyphicon glyphicon-user"></span> Settings</a></li>
-                    <li><a href = "logout_script.php"><span class = "glyphicon glyphicon-log-in"></span> Logout</a></li>
-                    ?>
-                    <?php
-                } else {
-                    ?>
-                    <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                        <?php
-                    }
-                    ?>
             </ul>
         </div>
     </div>
