0ef6d5aa3d049143dc2876cbb059aab52dc09842
Shreshtha Tola <44457357+shreshthatola26@users.noreply.github.com>
2020-08-30 21:09:26 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,45 @@
-. 
+. <!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Login | Life Style Store</title>
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        
+        <div id="content">
+            <div class="container-fluid decor_bg" id="login-panel">
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-4">
+                        <div class="panel panel-primary" >
+                            <div class="panel-heading">
+                                <h4>LOGIN</h4>
+                            </div>
+                            <div class="panel-body">
+                                <p class="text-warning"><i>Login to make a purchase</i><p>
+                                <form action="login_submit.php" method="POST">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control"  placeholder="Email" name="e-mail" required = "true">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" placeholder="Password" name="password" required = "true">
+                                    </div>
+                                    <button type="submit" name="submit" class="btn btn-primary">Login</button><br><br>
+                             
+                                </form><br/>
+                            </div>
+                            <div class="panel-footer"><p>Don't have an account? <a href="signup.php">Register</a></p></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
