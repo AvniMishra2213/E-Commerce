30c74f47c962f64276cb30eba9f12e6576174831
Avni Mishra <65212683+AvniMishra2213@users.noreply.github.com>
2020-09-01 07:59:57 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,1 +1,25 @@
-
+<?php
+require("includes/common.php");
+if (isset($_SESSION['email'])) {
+    header('location: products.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Signup | Life Style Store</title>
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <?php include 'includes/header.php'; ?>
+        <div class="container-fluid decor_bg" id="content">
+            <div class="row">
+                <div class="container">
+                    <div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3">
+                        <h2>SIGN UP</h2>
+                        <form  action="signup_script.php" method="POST">
+           
