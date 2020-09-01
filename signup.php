0bc2eb5661f5748911b372772da70d05fd53371d
Avni Mishra <65212683+AvniMishra2213@users.noreply.github.com>
2020-09-01 08:00:13 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -22,4 +22,31 @@ if (isset($_SESSION['email'])) {
                     <div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3">
                         <h2>SIGN UP</h2>
                         <form  action="signup_script.php" method="POST">
+                                             <div class="form-group">
+                                <input class="form-control" placeholder="Name" name="name"  required = "true" pattern="^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$">
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control"  placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"  name="e-mail" required = "true"><?php echo $_GET['m1']; ?>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" placeholder="Password" pattern=".{6,}" name="password" required = "true">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control"  placeholder="Contact" maxlength="10" size="10" name="contact" required = "true"><?php echo $_GET['m2']; ?>
+                            </div>
+                            <div class="form-group">
+                                <input  type="text" class="form-control"  placeholder="City" name="city" required = "true">
+                            </div>
+                            <div class="form-group">
+                                <input  type="text" class="form-control"  placeholder="Address" name="address" required = "true">
+                            </div>
+                            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include "includes/footer.php"; ?>
+    </body>
+</html>
            
